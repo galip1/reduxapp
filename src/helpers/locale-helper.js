43ac0cc258ed0ddc469aff6f56b891b130f6d9bd
galip <galipts1@gmail.com>
2023-01-22 08:27:43 +0300

    v1

---
+++ src/helpers/locale-helper.js
@@ -0,0 +1,22 @@
+import store from "../store";
+import { setLang } from "../store/slices/locale-slice";
+
+export const langs = [
+  { title: "English", code: "en", country: "gb" },
+  { title: "Türkçe", code: "tr", country: "tr" },
+  { title: "Francais", code: "fr", country: "fr" },
+];
+
+export const getCurrentLang = () => {
+  return store.getState().locale.lang;
+};
+
+export const setCurrentLang = (lang) => {
+  store.dispatch(setLang(lang));
+};
+
+export const $t = (key) => {
+  const currentLang = getCurrentLang();
+  const langFile = require(`../helpers/langs/${currentLang.code}.json`);
+  return langFile[key];
+};
